e09a424e4acaf21daf4f217f6f8cdaa6877a7a89
Abhilove Goyal <140530756+Abhilove-Goyal@users.noreply.github.com>
2025-12-16 16:54:04 +0530

    Update save_submission.py

--- outputs/save_submission.py
+++ outputs/save_submission.py
@@ -5,14 +5,13 @@ import io
 SUBMISSION_FILE = "outputs/submission.csv"
 # csv_data = save_submission(query, results)
 def save_submission(query, results):
-    """
-    Returns CSV content as string in SHL-required format
-    """
     output = io.StringIO()
     writer = csv.writer(output)
 
     for r in results:
-        writer.writerow([query, r["assessment_url"]])
+        url = r.get("assessment_url") or r.get("url")
+        if url:
+            writer.writerow([query, url])
 
     return output.getvalue()
 
